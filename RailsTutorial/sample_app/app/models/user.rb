1f0ae6598502dd85e4810edea01b106932e84256
h0wXD <h0wXD@users.noreply.github.com>
2015-03-05 19:45:29 +0100

    Chapter 9.

--- RailsTutorial/sample_app/app/models/user.rb
+++ RailsTutorial/sample_app/app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   validates(:name, presence: true, length: {maximum: 50})
   validates(:email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX})
   has_secure_password
-  validates(:password, length: {minimum: 6})
+  validates(:password, length: {minimum: 6}, allow_blank: true)
 
   class << self
     # Returns the hash digest of the given string.
--- RailsTutorial/sample_app/test/controllers/users_controller_test.rb
+++ RailsTutorial/sample_app/test/controllers/users_controller_test.rb
@@ -1,9 +1,70 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+  def setup
+    @user = users(:one)
+    @another_user = users(:two)
+  end
+
   test 'should get new' do
     get :new
     assert_response :success
+  end
+
+  test 'should redirect edit when not logged in' do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect update when not logged in' do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect edit when logged in as wrong user' do
+    log_in_as(@another_user)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect update when logged in as wrong user' do
+    log_in_as(@another_user)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect index when not logged in' do
+    get :index
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when logged in as a non-admin' do
+    log_in_as(@another_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
+
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@another_user)
+    assert_not @another_user.admin?
+    patch :update, id: @another_user, user: { password:              'password',
+                                              password_confirmation: 'password',
+                                              admin: true }
+    assert_not @another_user.reload.admin?
   end
 
 end
