dc5ee03500602c864fab575ce47bab3988de69e3
h0wXD <h0wXD@users.noreply.github.com>
2015-03-07 21:30:43 +0100

    Chapter 11 - part 1.

--- RailsTutorial/sample_app/app/controllers/users_controller.rb
+++ RailsTutorial/sample_app/app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 #    debugger
+    @per_page = params[:per_page] || 3
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: @per_page)
   end
 
   def new
@@ -51,15 +53,6 @@ class UsersController < ApplicationController
 private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
-    end
   end
 
   # Confirms the correct user.
